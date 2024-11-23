5c28f3c824aea56b4e672e1ac5f2eca4d8151a72
Ben <me@benspace.xyz>
2024-11-23 16:43:57 +0700

    fix: handle if session directory not exist

--- utils/atomics.ts
+++ utils/atomics.ts
@@ -1,4 +1,5 @@
 import path from "path";
+import fs from "fs";
 
 export const SESSION_DIR = "session";
 export const STORE_DIR = "baileys_store";
@@ -7,10 +8,17 @@ export const sanitizeSessionName = (name: string): string => {
   return name.replace(/ /g, "_").replace(/[$&+,:;=?@#|'<>.^*()%!-]/g, "-");
 };
 
+const checkAndCreateDir = (dirName: string) => {
+  const isAvailable = fs.existsSync(dirName);
+  if (!isAvailable) fs.mkdirSync(dirName);
+};
+
 export const getStorePath = (sessionName: string): string => {
+  checkAndCreateDir(STORE_DIR);
   return path.join(STORE_DIR, `${sessionName}.json`);
 };
 
 export const getSessionPath = (sessionName: string): string => {
+  checkAndCreateDir(STORE_DIR);
   return path.join(SESSION_DIR, `baileys_${sessionName}`);
 };
